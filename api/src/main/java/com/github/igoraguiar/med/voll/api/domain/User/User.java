721731d31d52dc45f4cd638ab065578ded5f644d
igor-aguiar <desenvigor@gmail.com>
2023-08-20 21:55:08 -0300

    add spring security/ add user class

---
+++ api/src/main/java/com/github/igoraguiar/med/voll/api/domain/User/User.java
@@ -0,0 +1,22 @@
+package com.github.igoraguiar.med.voll.api.domain.User;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Entity(name = "user")
+@Table(name = "users")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String login;
+    private String password;
+}
---
+++ api/src/main/resources/db/migration/V4__create-table-users.sql
@@ -0,0 +1,5 @@
+create table users(
+    id bigint primary key auto_increment not null ,
+    name varchar(100) not null ,
+    password varchar(255) not null
+)
